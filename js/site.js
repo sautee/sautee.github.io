cf20fcd3c209191bde751b170a089dfcaec005f9
Andrew Chacko <39371787+sautee@users.noreply.github.com>
2020-05-03 19:46:51 +0300

    Update site.js

--- js/site.js
+++ js/site.js
@@ -1,9 +1,1 @@
-let date = new Date();
-const paragraph = document.createElement('p');
-
-paragraph.textContent = date;
-document.body.appendChild(paragraph);
-
-const container = document.getElementsByClassName('container');
-paragraph.textContent = container[0].innerText;
-document.body.appendChild(paragraph);
+//nothing here yet
