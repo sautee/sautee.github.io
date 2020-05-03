eb31fa15424d6bc373eb29f3b2143905349045ff
Andrew Chacko <39371787+sautee@users.noreply.github.com>
2020-05-03 19:43:49 +0300

    Update site.js

--- js/site.js
+++ js/site.js
@@ -3,3 +3,7 @@ const paragraph = document.createElement('p');
 
 paragraph.textContent = date;
 document.body.appendChild(paragraph);
+
+const container = document.getElementsByClassName('container');
+paragraph.textContent = container[0].innerText;
+document.body.appendChild(paragraph);
